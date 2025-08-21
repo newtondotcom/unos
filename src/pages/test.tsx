698511c15f78ef4d752f85e00b923e6ebade64bd
newtondotcom <asphalt9fr@gmail.com>
2025-08-21 12:41:11 +0000

    chore: test page

---
+++ src/pages/test.tsx
@@ -0,0 +1,11 @@
+import GradientButton from "@/components/kokonutui/gradient-button";
+
+const TestPage = () => {
+    return (
+        <main className="min-h-screen">
+            < GradientButton />
+        </main>
+    );
+    }
+
+export default TestPage;
